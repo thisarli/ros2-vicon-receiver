1777dece603f91700e1d3812bd5895b3ed609455
tilman <tth21@ic.ac.uk>
2023-05-15 16:40:22 +0000

    spin some

--- vicon_receiver/src/vicon_receiver/communicator.cpp
+++ vicon_receiver/src/vicon_receiver/communicator.cpp
@@ -127,7 +127,7 @@ void Communicator::get_frame()
                     {
                         std::cout << "Pub is ready" << std::endl;
                         pub.publish(current_position);
-                        rclcpp::spin_once()
+                        rclcpp::spin_some()
                     }
                 }
                 else
